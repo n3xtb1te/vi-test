af71827b72b9fa588bc7ea46f8197866bc8ad98f
n3xtb1te <168577163+n3xtb1te@users.noreply.github.com>
2024-09-26 16:16:58 +0300

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,5 +25,6 @@ module.exports = {
     quotes: ['error', 'single'],
     indent: ['error', 2],
     'space-infix-ops': ['error', { 'int32Hint': false }],
+    'no-unused-vars': ['error', { 'args': 'none' }],
   }
 };
