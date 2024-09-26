f205be5d57e7d193d9fb02b86cd5bd7605aeafe1
n3xtb1te <168577163+n3xtb1te@users.noreply.github.com>
2024-09-26 13:09:42 +0300

    apply functions.ts with eslint

--- src/functions.ts
+++ src/functions.ts
@@ -1,32 +1,32 @@
-import { HUNDRED_PERCENT, LOW_PERCENT, MEDIUM_PERCENT, MILLISECONDS_IN_SECOND } from './constants'
-import { ProgressColorClass } from './types'
+import { HUNDRED_PERCENT, LOW_PERCENT, MEDIUM_PERCENT, MILLISECONDS_IN_SECOND } from './constants';
+import { ProgressColorClass } from './types';
 
 export function formatSecondsWithSign(seconds: number): string {
-  return `${seconds >= 0 ? '+' : '-'}${formatSeconds(seconds)}`
+  return `${seconds >= 0 ? '+' : '-'}${formatSeconds(seconds)}`;
 }
 
 export function formatSeconds(seconds: number): string {
-  const date = new Date()
+  const date = new Date();
 
-  date.setTime(Math.abs(seconds) * MILLISECONDS_IN_SECOND)
+  date.setTime(Math.abs(seconds) * MILLISECONDS_IN_SECOND);
 
-  const utc = date.toUTCString()
+  const utc = date.toUTCString();
 
-  return utc.substring(utc.indexOf(':') - 2, utc.indexOf(':') + 6)
+  return utc.substring(utc.indexOf(':') - 2, utc.indexOf(':') + 6);
 }
 
 export function normalizeSelectValue(value: string | null): number | string | null {
-  return value === null || isNaN(Number(value)) ? value : +value
+  return value === null || isNaN(Number(value)) ? value : +value;
 }
 
 export function getProgressColorClass(percentage: number): ProgressColorClass {
-  if (percentage < LOW_PERCENT) return ProgressColorClass.RED
-  if (percentage < MEDIUM_PERCENT) return ProgressColorClass.YELLOW
-  if (percentage < HUNDRED_PERCENT) return ProgressColorClass.BLUE
+  if (percentage < LOW_PERCENT) return ProgressColorClass.RED;
+  if (percentage < MEDIUM_PERCENT) return ProgressColorClass.YELLOW;
+  if (percentage < HUNDRED_PERCENT) return ProgressColorClass.BLUE;
 
-  return ProgressColorClass.GREEN
+  return ProgressColorClass.GREEN;
 }
 
 export function id(): string {
-  return Date.now().toString(36) + Math.random().toString(36).substring(2)
+  return Date.now().toString(36) + Math.random().toString(36).substring(2);
 }
