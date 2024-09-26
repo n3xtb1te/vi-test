1cc117edb11facf8ce0769f09b2fc7ab2662aa9e
n3xtb1te <168577163+n3xtb1te@users.noreply.github.com>
2024-09-26 16:17:28 +0300

    update router.ts with eslint

--- src/router.ts
+++ src/router.ts
@@ -1,32 +1,32 @@
-import { ref, type Component } from 'vue'
-import { NAV_ITEMS } from './constants'
-import TheActivities from './pages/TheActivities.vue'
-import TheProgress from './pages/TheProgress.vue'
-import TheTimeline from './pages/TheTimeline.vue'
-import { PageName } from './types'
+import { ref, type Component } from 'vue';
+import { NAV_ITEMS } from './constants';
+import TheActivities from './pages/TheActivities.vue';
+import TheProgress from './pages/TheProgress.vue';
+import TheTimeline from './pages/TheTimeline.vue';
+import { PageName } from './types';
 
 export const routes: Record<PageName, Component> = {
   [PageName.TIMELINE]: TheTimeline,
   [PageName.ACTIVITIES]: TheActivities,
   [PageName.PROGRESS]: TheProgress
-}
+};
 
-export const currentPage = ref<PageName>(normalizePageHash())
+export const currentPage = ref<PageName>(normalizePageHash());
 
 export function navigate(page: PageName): void {
-  document.body.scrollIntoView()
+  document.body.scrollIntoView();
 
-  currentPage.value = page
+  currentPage.value = page;
 }
 
 export function normalizePageHash(): PageName {
-  const page = window.location.hash.slice(1)
+  const page = window.location.hash.slice(1);
 
   if (NAV_ITEMS.some((navItem): boolean => navItem.page === page)) {
-    return page as PageName
+    return page as PageName;
   }
 
-  window.location.hash = PageName.TIMELINE
+  window.location.hash = PageName.TIMELINE;
 
-  return PageName.TIMELINE
+  return PageName.TIMELINE;
 }
