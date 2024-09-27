3e230ef0125f34895706c0a55aa35caf3f41abc7
n3xtb1te <168577163+n3xtb1te@users.noreply.github.com>
2024-09-27 13:14:43 +0300

    update timeline-items tests with constants instead of hardcoding

--- test/timeline-items.test.ts
+++ test/timeline-items.test.ts
@@ -5,20 +5,21 @@ import {
   calculateTrackedActivitySeconds 
 } from '../src/timeline-items';
 import { Activity, Hour, TimelineItem } from '../src/types';
+import { SECONDS_IN_HOUR } from '../src/constants';
 
-describe.skip('update timeline item', () => {
+describe('update timeline item', () => {
   it('v1', () => {
     const timelineItem: TimelineItem = {
       hour: 1,
       activityId: '1',
-      activitySeconds: 0,
+      activitySeconds: SECONDS_IN_HOUR * 0,
       isActive: false
     };
 
     const updatedFields: TimelineItem = {
       hour: 2,
       activityId: '2',
-      activitySeconds: 3600,
+      activitySeconds: SECONDS_IN_HOUR * 1,
       isActive: true
 
     };
@@ -30,7 +31,7 @@ describe.skip('update timeline item', () => {
   });
 });
 
-describe.skip('reset timeline item activities', () => {
+describe('reset timeline item activities', () => {
   it('v1', () => {
     const date = new Date('2024-04-10T02:00:00');
 
@@ -39,32 +40,32 @@ describe.skip('reset timeline item activities', () => {
     const trainingActivity: Activity = {
       id: '1',
       name: 'Training',
-      secondsToComplete: 3600
+      secondsToComplete: SECONDS_IN_HOUR * 1
     };
 
     const readingActivity: Activity = {
       id: '2',
       name: 'Reading',
-      secondsToComplete: 7200
+      secondsToComplete: SECONDS_IN_HOUR * 2
     };
 
     const timelineItems: TimelineItem[] = [
       {
         hour: 1,
         activityId: trainingActivity.id,
-        activitySeconds: 3600,
+        activitySeconds: SECONDS_IN_HOUR * 1,
         isActive: false
       },
       {
         hour: 2,
         activityId: trainingActivity.id,
-        activitySeconds: 7200,
+        activitySeconds: SECONDS_IN_HOUR * 2,
         isActive: true
       },
       {
         hour: 3,
         activityId: readingActivity.id,
-        activitySeconds: 7200,
+        activitySeconds: SECONDS_IN_HOUR * 2,
         isActive: true
       }
     ];
@@ -75,19 +76,19 @@ describe.skip('reset timeline item activities', () => {
       {
         hour: 1,
         activityId: null,
-        activitySeconds: 0,
+        activitySeconds: SECONDS_IN_HOUR * 0,
         isActive: false
       },
       {
         hour: date.getHours() as Hour,
         activityId: null,
-        activitySeconds: 7200,
+        activitySeconds: SECONDS_IN_HOUR * 2,
         isActive: true
       },
       {
         hour: 3,
         activityId: readingActivity.id,
-        activitySeconds: 7200,
+        activitySeconds: SECONDS_IN_HOUR * 2,
         isActive: true
       }
     ]);
@@ -96,37 +97,37 @@ describe.skip('reset timeline item activities', () => {
   });
 });
 
-describe.skip('calculate tracked activity seconds', () => {
+describe('calculate tracked activity seconds', () => {
   it('v1', () => {
     const trainingActivity: Activity = {
       id: '1',
       name: 'Training',
-      secondsToComplete: 3600
+      secondsToComplete: SECONDS_IN_HOUR * 1
     };
     
     const readingActivity: Activity = {
       id: '2',
       name: 'Reading',
-      secondsToComplete: 7200
+      secondsToComplete: SECONDS_IN_HOUR * 2
     };
     
     const timelineItems: TimelineItem[] = [
       {
         hour: 1,
         activityId: trainingActivity.id,
-        activitySeconds: 3600,
+        activitySeconds: SECONDS_IN_HOUR * 1,
         isActive: false
       },
       {
         hour: 2,
         activityId: trainingActivity.id,
-        activitySeconds: 7200,
+        activitySeconds: SECONDS_IN_HOUR * 2,
         isActive: true
       },
       {
         hour: 3,
         activityId: readingActivity.id,
-        activitySeconds: 7200,
+        activitySeconds: SECONDS_IN_HOUR * 2,
         isActive: true
       }
     ];
@@ -134,7 +135,7 @@ describe.skip('calculate tracked activity seconds', () => {
     const trackedTrainingActivitySeconds = calculateTrackedActivitySeconds(timelineItems, trainingActivity);
     const trackedReadingActivitySeconds = calculateTrackedActivitySeconds(timelineItems, readingActivity);
     
-    expect(trackedTrainingActivitySeconds).toBe(10800);
-    expect(trackedReadingActivitySeconds).toBe(7200);
+    expect(trackedTrainingActivitySeconds).toBe(SECONDS_IN_HOUR * 3);
+    expect(trackedReadingActivitySeconds).toBe(SECONDS_IN_HOUR * 2);
   });
 });
